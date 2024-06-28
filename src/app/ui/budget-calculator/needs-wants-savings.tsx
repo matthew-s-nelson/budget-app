35f699c3043073ac5faf4adb3981cc6968191d17
Matthew Nelson <matthewbuzz@gmail.com>
2024-06-28 11:29:03 -0600

    Round percentages

--- src/app/ui/budget-calculator/needs-wants-savings.tsx
+++ src/app/ui/budget-calculator/needs-wants-savings.tsx
@@ -1,4 +1,4 @@
-import {formatNumWithCommas} from '@/app/utils/formatting'
+import {formatNumWithCommas, twoDecimalPlaces} from '@/app/utils/formatting'
 
 export default function NWSTable( { needs, wants, savings }) {
     const total = needs + wants + savings;
@@ -6,9 +6,9 @@ export default function NWSTable( { needs, wants, savings }) {
     let wantsPercentage = 30;
     let savingsPercentage = 20;
     if (total > 0) {
-        needsPercentage = needs / total * 100;
-        wantsPercentage = wants / total * 100;
-        savingsPercentage = savings / total * 100;
+        needsPercentage = Math.round(needs / total * 100);
+        wantsPercentage = Math.round(wants / total * 100);
+        savingsPercentage = Math.round(savings / total * 100);
     }
     return (
         <table>
