1db989dffd1251145447d3b17a3dfd90546dc79b
Matthew Nelson <matthewbuzz@gmail.com>
2024-08-16 13:58:23 -0600

    Add website name to sidenave

--- src/components/ui/dashboard/sidenav.tsx
+++ src/components/ui/dashboard/sidenav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import NavLinks from '@/components/ui/dashboard/nav-links';
 import { logout } from '@/auth';
 import { redirect } from 'next/navigation';
@@ -14,14 +15,14 @@ export default function SideNav() {
 
     return (
         <div className='flex h-full flex-col px-3 py-4 md:pd-2'>
-            <Link
-                className='mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40'
-                href='/'
-            >
-                <div className='w-32 text-white md:w-40'>
-                    
-                </div>
-            </Link>
+        <Link
+            className='mb-2 flex h-20 items-center justify-center rounded-md bg-blue-600 p-4 md:h-40'
+            href='/'
+        >
+            <div className='w-32 text-white text-center dynamic-text md:w-40'>
+                <b>Budget MB</b>
+            </div>
+        </Link>
             <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
                 <NavLinks />
                 <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div>
