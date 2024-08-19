c5e10fc787cd9c51b012a0f441922f7ece450270
Matthew Nelson <matthewbuzz@gmail.com>
2024-08-19 09:20:59 -0600

    Style the categories list

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -15,22 +15,39 @@ export default async function Page() {
                 <button type="submit" className="btn-primary">Create</button>
             </form>
             <div>
+            <table className="table-auto w-full">
+            <thead>
+                <tr>
+                    <th>Category</th>
+                    <th>Budget</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
                 {categories.map(category => (
-                    <div key={category.id} className="flex items-center space-x-2 my-2">
-                        <span>{category.name}</span>
-                        <form action={setBudget}>
-                            <input name="id" type="hidden" value={category.id} />
-                            <input type="number" name="budget" className="input-text" placeholder="Enter the budget" value={category.annual_budget} required />
-                            <select name="type" className="input-text" required>
-                                <option value="yearly">Yearly</option>
-                                <option value="monthly">monthly</option>
-                                <option value="weekly">Weekly</option>
-                            </select>
-                            <button type="submit" className="btn-primary">Set Budget</button>
-                        </form>
-                        <DeleteCategory id={category.id} />
-                    </div>
+                    <tr key={category.id}>
+                        <td>{category.name}</td>
+                        <td colSpan={3}>
+                            <form action={setBudget}>
+                                <input name="id" type="hidden" value={category.id} />
+                                <input type="number" name="budget" className="input-text mx-2" placeholder="Enter the budget" value={category.annual_budget} required />
+                                <select name="type" className="input-text mx-2" required>
+                                    <option value="yearly">Yearly</option>
+                                    <option value="monthly">monthly</option>
+                                    <option value="weekly">Weekly</option>
+                                </select>
+                                <button type="submit" className="btn-primary w-32 mx-2">Set Budget</button>
+                            </form>
+                        </td>
+                        <td>
+                            <DeleteCategory id={category.id} />
+                        </td>
+                    </tr>
                 ))}
+            </tbody>
+        </table>
             </div>
         </div>
     );
